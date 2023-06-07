9ed7f824211d567c680020d29bd9d5317d0515eb
01fe21bec351 <130679366+01fe21bec351@users.noreply.github.com>
2023-06-07 10:45:35 +0530

    Add files via upload

---
+++ tressposttraverse.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Binary Tree Node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new Node
+struct Node* newNode(int data)
+{
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to insert a new Node into the Binary Tree
+struct Node* insert(struct Node* node, int data)
+{
+    if (node == NULL)
+        return newNode(data);
+
+    if (data < node->data)
+        node->left = insert(node->left, data);
+    else if (data > node->data)
+        node->right = insert(node->right, data);
+
+    return node;
+}
+
+// Function to perform Post-order Traversal of the Binary Tree
+void postOrderTraversal(struct Node* node)
+{
+    if (node == NULL)
+        return;
+
+    postOrderTraversal(node->left);
+    postOrderTraversal(node->right);
+    printf("%d ", node->data);
+}
+
+// Driver code
+int main()
+{
+    struct Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Post-order Traversal: ");
+    postOrderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
+
